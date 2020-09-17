f03ee1a2ccc0170b5d2ccd012affe965d39fc1a8
Eric Weiss <ericweiss98@tamu.edu>
2020-09-17 10:23:28 -0500

    changed a migration

--- db/migrate/20200910162104_alter_book.rb
+++ db/migrate/20200910162104_alter_book.rb
@@ -1,8 +1,8 @@
 class AlterBook < ActiveRecord::Migration[5.1]
   def up
-	change_column("Books", "Title", :string, :default => "Default")
-	change_column("Books", "Genre", :string, :default => "Default")
-	change_column("Books", "Author", :string, :default => "Default")
+	change_column(:books, "Title", :string, :default => "Default")
+	change_column(:books, "Genre", :string, :default => "Default")
+	change_column(:books, "Author", :string, :default => "Default")
   end
 
 end
